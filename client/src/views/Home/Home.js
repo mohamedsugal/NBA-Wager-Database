2fbaca4e3bcee306929102ebbdfa421706402bd3
luisaparicioufl <44217585+luisaparicioufl@users.noreply.github.com>
2020-03-21 21:27:41 -0400

    bootstrap installed

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import logo from '../../assets/logo.svg';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from "react-bootstrap/Container"
 import './Home.css';
 
 function Home() {
     return (
         <div className="App">
+            <Jumbotron fluid>
+                <Container>
+                    <h1>Fluid jumbotron</h1>
+                    <p>
+                    This is a modified jumbotron that occupies the entire horizontal space of
+                    its parent.
+                    </p>
+                </Container>
+            </Jumbotron>
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                </a>
+            
             </header>
         </div>
     );
